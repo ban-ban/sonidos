09d24375a39491d8099c67835a35f9ffadfc963e
ban-ban <albancassus@gmail.com>
2018-12-15 11:04:06 +0100

    first commit

---
+++ src/data/data.animales.ts
@@ -0,0 +1,65 @@
+export const ANIMALES = [
+	{
+		nombre :"cheval",
+		imagen : "assets/animales/course1.jpg",
+		audio : "assets/sonidos/166.mp3",
+		duracion : 4,
+		reproduciendo : false
+	},
+	{
+		nombre :"lion",
+		imagen : "assets/animales/course2.jpg",
+		audio : "assets/sonidos/2376.mp3",
+		duracion : 2,
+		reproduciendo : false
+	},
+	{
+		nombre :"grive",
+		imagen : "assets/animales/course3.jpg",
+		audio : "assets/sonidos/2379.mp3",
+		duracion : 4,
+		reproduciendo : false
+	},
+	{
+		nombre :"mouton",
+		imagen : "assets/animales/course4.jpg",
+		audio : "assets/sonidos/11652.mp3",
+		duracion : 4,
+		reproduciendo : false
+	},
+	{
+		nombre :"poisson",
+		imagen : "assets/animales/course5.jpg",
+		audio : "assets/sonidos/13298.mp3",
+		duracion : 4,
+		reproduciendo : false
+	},
+	{
+		nombre :"poule",
+		imagen : "assets/animales/course6.jpg",
+		audio : "assets/sonidos/13303.mp3",
+		duracion : 4,
+		reproduciendo : false
+	},
+	{
+		nombre :"chien",
+		imagen : "assets/animales/course7.jpg",
+		audio : "assets/sonidos/20030.mp3",
+		duracion : 4,
+		reproduciendo : false
+	},
+	{
+		nombre :"canard",
+		imagen : "assets/animales/course8.jpg",
+		audio : "assets/sonidos/20045.mp3",
+		duracion : 4,
+		reproduciendo : false
+	},
+	{
+		nombre :"ours",
+		imagen : "assets/animales/course9.jpg",
+		audio : "assets/sonidos/22202.wav",
+		duracion : 4,
+		reproduciendo : false
+	}
+]
